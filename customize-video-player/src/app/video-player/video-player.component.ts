5be4f1ccc30af0668a8d76b00af170458587ddb7
Shivam Giri <shivamgiri2015@gmail.com>
2023-06-15 20:07:16 +0530

    Scafoldding

---
+++ customize-video-player/src/app/video-player/video-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cvp-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss']
+})
+export class VideoPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
